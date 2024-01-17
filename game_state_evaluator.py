7864bdae695ec62c9e7a0088d73395dc5fe1856b
mahsa-nazari <mahsamaleki.n@gmail.com>
2024-01-18 00:28:06 +0100

    modify scoring

--- game_state_evaluator.py
+++ game_state_evaluator.py
@@ -10,18 +10,16 @@ class GameStateEvaluator:
         A higher score typically means a more favorable state for Pac-Man.
         """
         # Example scoring logic: Pac-Man should avoid ghosts
-        try:
-            score -= 5 * self.calculate_pacman_ghost_distance(game_board)
-        except: score = 0 - 5 * self.calculate_pacman_ghost_distance(game_board)
-        # Add more scoring logic here based on game state
-        return score
+        self.score -= 5 * self.calculate_pacman_ghost_distance(game_board)
+        return self.score
 
     def calculate_pacman_ghost_distance(self, game_board):
         distances = []
-        print('game_board.ghosts_positions', game_board.ghosts_positions)
         for ghost_pos in game_board.ghosts_positions:
             distance = abs(game_board.pacman_position[0] - ghost_pos[0]) + abs(game_board.pacman_position[1] - ghost_pos[1])
             distances.append(distance)
-            print('distances',distances)
 
         return min(distances) if distances else 0
+    
+    def print_scores(self):
+        print(self.score)
